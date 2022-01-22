94c9ee24adabffc7c2b10dd11ed193bdef0e6909
manohar kumar <dev.manoharkumar@gmail.com>
2022-01-22 12:03:24 +0530

    project init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {Sorter} from "./Sorter";
+import {NumbersCollection} from "./NumbersCollection";
+
+const numberCollection = new NumbersCollection([10, 3, -5, 0])
+const sorter = new Sorter(numberCollection)
+sorter.sort();
+console.log(numberCollection.data)
